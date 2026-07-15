96a49e3198db7a22c9bfd0afc0ebdc203be14d69
rarafahri18-glitch <rarafahri18@gmail.com>
2026-07-15 21:08:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import random
 
 st.title("Rating seberapa tinggi selera muasik loe")
 
-import C:\Users\LENOVO\.vscode\code\rating musik.py
+musik = st.teks.input("Masukkan musik ke sukaan loe")
+if st.butoon("lihat Rating"):
+  angka = random.Radiant(1, 10)
+
+if angka < 3:
+  Rating = "kurang bagus bro"
+elif angka < 7:
+  Rating = "bBagus"
+else:
+  Rating = "Elitis kah?"
+
+st.write(f"Musik kesukaan loe: {musik}")
+st.success(f"Rating: {Rating}")
