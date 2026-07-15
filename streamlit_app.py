02223edf3a9afd458ce2d0c86ac8aafb4e5d9aa3
rarafahri18-glitch <rarafahri18@gmail.com>
2026-07-15 20:48:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ musik_kesukaan = input("Masukkan musik kesukaan Anda: ")
 import random
 angka_acak = random.randint(1, 10)
 rating = "kurang bagus" if angka_acak < 3 else "cukup bagus" if angka_acak < 7 else "sangat bagus"
-print(f"Musik kesukaan Anda adalah {musik_kesukaan}. Rating musik ini adalah {rating}.")
+print(f"Musik kesukaan Anda adalah {musik_kesukaan}. Rating musik ini adalah {rating}.")  
