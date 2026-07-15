b334a8beaf8e792d534ce31ff9ddae2e893f0b95
rarafahri18-glitch <rarafahri18@gmail.com>
2026-07-15 20:43:24 +0700

    Update app title and remove introductory text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title(Rating seberapa tinggi selera muasik loe)
