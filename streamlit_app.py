aa5de34bb7afe3fc7db67c1b5badfeca49235ae4
rarafahri18-glitch <rarafahri18@gmail.com>
2026-07-15 20:56:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,4 @@ import streamlit as st
 
 st.title("Rating seberapa tinggi selera muasik loe")
 
-constant.musik_kesukaan = input("Masukkan musik kesukaan Anda: ")
-import random
-constant.angka_acak = random.randint(1, 10)
-constant.rating = "kurang bagus" if angka_acak < 3 else "cukup bagus" if angka_acak < 7 else "sangat bagus"
-print(f"Musik kesukaan Anda adalah {musik_kesukaan}. Rating musik ini adalah {rating}.")  
+import rating musik.py
